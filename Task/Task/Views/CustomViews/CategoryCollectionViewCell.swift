449b5eb97e324ad7464dc6d1fdd87199f46952a7
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-10 14:35:02 +0200

    Customize CategoryCollectionViewCell

---
+++ Task/Task/Views/CustomViews/CategoryCollectionViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Task
+//
+//  Created by Eslam Ali  on 10/06/2022.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    
+    static let identifier = "CategoryCollectionViewCell"
+    
+    private let CategoryImageView : UIImageView = {
+       let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.layer.cornerRadius = 20
+        image.image = UIImage(systemName: "person")
+        image.translatesAutoresizingMaskIntoConstraints = false
+
+        return image
+    }()
+    
+    public let titleLabel : UILabel = {
+        let label =  UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.textColor = .label
+        label.textAlignment = .center
+        label.text = "Category Title"
+        label.font = UIFont(name: "Cairo-Bold", size: 15)
+//        label.font = .systemFont(ofSize: 15, weight: .bold)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    
+    //MARK:- Initilizers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemBackground
+        setupLayouts()
+        ConfigureConstraints()
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+  //MARK:- Layouts / Constraints
+    private func setupLayouts(){
+        
+       
+        contentView.addSubview(CategoryImageView)
+        contentView.addSubview(titleLabel)
+       
+//        contentView.layer.borderWidth = 0.5
+//        contentView.layer.cornerRadius = 5
+//        contentView.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        
+    }
+    
+    private func ConfigureConstraints(){
+        NSLayoutConstraint.activate([
+            
+          
+            
+            
+            CategoryImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            CategoryImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            CategoryImageView.widthAnchor.constraint(equalToConstant: 40),
+            CategoryImageView.heightAnchor.constraint(equalToConstant: 40),
+            
+
+
+            titleLabel.topAnchor.constraint(equalTo: CategoryImageView.bottomAnchor,constant: 1),
+
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+
+        ])
+    }
+    
+    
+    
+}
