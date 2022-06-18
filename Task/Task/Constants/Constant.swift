edd247dc769540e92b20005324453d8fd29d2946
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-18 23:54:54 +0200

    Add Custom Fiont

---
+++ Task/Task/Constants/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Task
+//
+//  Created by Eslam Ali  on 10/06/2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseUrl = "http://satafood.codesroots.com:3000/"
+    
+}
